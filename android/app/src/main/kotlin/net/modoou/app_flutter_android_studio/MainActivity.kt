f7849df605f341afcf6f9bd14426c5a6be9e9812
macielsousadasilva <xdtecnologia@gmail.com>
2019-12-14 17:24:19 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/net/modoou/app_flutter_android_studio/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.modoou.app_flutter_android_studio
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
